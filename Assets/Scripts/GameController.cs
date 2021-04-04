5693ed708c12e67f3d0f5c3d27775f4677ffbc92
Carl Markham <carl.markham@elasticpath.com>
2021-04-04 13:19:49 +0100

    Add button to spawn sheep

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject sheep;
-    List<GameObject> totalSheep;
-    
+    int totalSheep;
+
+    Text sheepCountText;
+
+    private void Start()
+    {
+        sheepCountText = GameObject.Find("Sheep Count").GetComponent<Text>();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            GameObject newSheep = Instantiate(sheep, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
-            totalSheep.Add(newSheep);
-        }
+        totalSheep = GameObject.FindGameObjectsWithTag("Sheep").Length;
+        sheepCountText.text = totalSheep.ToString();
+    }
+
+    public void SpawnSheep()
+    {
+        Debug.Log("SPAWN");
+        Instantiate(sheep, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
     }
 }
