82c00c6b3da22b448d76aa006fd3f877838357f1
Carl Markham <carl.markham@elasticpath.com>
2021-12-18 14:27:55 +0000

    remove janky queue ui

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class UIController : MonoBehaviour {
     private Sheep selectedSheep;
-    private RectTransform CurrentActionRect;
-    private RectTransform NextActionsRect;
 
     private void Start() {
-        CurrentActionRect = GameObject.Find("Current Action").GetComponent<RectTransform>();
-        NextActionsRect = GameObject.Find("Next Actions").GetComponent<RectTransform>();
     }
 
     private void FixedUpdate () {
@@ -25,7 +19,6 @@ public class UIController : MonoBehaviour {
 
                     selectedSheep.transform.Find("Stats").gameObject.SetActive(true);
                 } else {
-                    EmptyQueueUI();
                     selectedSheep = null;
 
                     // Remove the outline from every sheep (expensive?)
@@ -39,50 +32,5 @@ public class UIController : MonoBehaviour {
                 }
             }
         }
-
-        UpdateSheepUI();
-    }
-
-    void EmptyQueueUI() {
-        foreach (Text existing in CurrentActionRect.GetComponentsInChildren<Text>()) {
-            Destroy(existing.gameObject);
-        }
-
-        foreach (Text existing in NextActionsRect.GetComponentsInChildren<Text>()) {
-            Destroy(existing.gameObject);
-        }
-    }
-
-    void UpdateSheepUI() {
-        if (selectedSheep == null) {
-            return;
-        }
-
-        EmptyQueueUI();
-
-        // Add the first action to the current action queue
-        Action first = selectedSheep.ActionQueue.CurrentAction;
-        Text text = CreateTextForAction(first);
-        text.transform.SetParent(CurrentActionRect);
-
-        // Add the rest to the next action queue
-        List<Action> queueActions = selectedSheep.ActionQueue.All();
-
-        for (int i = 1; i < queueActions.Count; i++) {
-            Action action = queueActions[i];
-            Text t = CreateTextForAction(action);
-            t.transform.SetParent(NextActionsRect);
-        }
-    }
-
-    Text CreateTextForAction(Action action) {
-        GameObject textGameObject = new GameObject();
-        Text t = textGameObject.AddComponent<Text>();
-        t.text = action.Type.ToString();
-        t.alignment = TextAnchor.MiddleRight;
-        t.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        t.fontSize = 14;
-
-        return t;
     }
 }
